539968b2e6ab7dacfd8a43729e5c4e85d8722023
Giovanni <giovanni@solone.it>
2019-09-10 11:31:33 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -35,7 +35,7 @@
 		
 	} elseif(strpos($text, "/version") === 0 || strpos($text, "/version@AmazonReferralBot")) {
 		
-		$response = "#AmznRefBot 0.1 - Per segnalare nuovi referral apri una issue su GitHub: https://github.com/gfscom/amznrefbot/issues (o se preferisci puoi sempre fare pull sul file execute.php)";
+		$response = "#AmznRefBot 0.2.100920191131 - Per segnalare nuovi referral apri una issue su GitHub: https://github.com/gfscom/amznrefbot/issues (o se preferisci puoi sempre fare pull sul file execute.php)";
 		
 	} else {}
 
